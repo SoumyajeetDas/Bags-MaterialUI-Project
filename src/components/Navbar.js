cca3900d5cccefec274cc8d56b167efaa5e6705a
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-12-18 20:17:09 +0530

    Header Completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    AppBar, Container, IconButton, Toolbar, Typography, Stack, Button
+} from '@mui/material';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div>
+            <AppBar position='static' sx={{
+                backgroundColor: "warning.light"
+            }}>
+                <Container fixed>
+                    <Toolbar>
+                        <IconButton color='inherit' disableRipple sx={{
+                            border: "2px solid #fff",
+                            mr: "10px"
+                        }}>
+                            <CurrencyBitcoinIcon fontSize="medium" />
+                        </IconButton>
+                        <Typography variant="h4" id="toolbar">
+                            Cryptos
+                        </Typography>
+                        <Stack direction="row" spacing={2} sx={{
+                            ml: "auto"
+                        }}>
+                            <Button id="button-style" color="inherit" sx={{
+                                fontSize: "19px",
+                                '&:hover': {
+                                    backgroundColor: "warning.main"
+                                }
+                            }}>Home</Button>
+                            <Button id="button-style" color="inherit" sx={{
+                                fontSize: "19px",
+                                '&:hover': {
+                                    backgroundColor: "warning.main"
+                                }
+                            }}>DashBoard</Button>
+
+                        </Stack>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
